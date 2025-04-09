64e37cd81e6a99fe3089173e1a84ab138df1c8fe
iii <isaacmartinez@MacBook-Pro-de-Isaac-2.local>
2025-04-08 20:19:44 -0600

    question 4

--- lib/airbnb.ex
+++ lib/airbnb.ex
@@ -1,40 +1,83 @@
 defmodule Airbnb do
-  require Logger
   require IO
 
-  def aggr_count_properties(filename) do
+  def offer_by_neighbourhood(filename) do
+    neighbourhood_index = 28
+    accommodates_index = 34
+    price_index = 40
+    min_row_length = Enum.max([neighbourhood_index, accommodates_index, price_index]) + 1
+
     try do
-      neighbourhood_index = 28
-      property_type_index = 32
-
-      file_stream = File.stream!(filename, [:read, :utf8], :line)
-      csv_stream = NimbleCSV.RFC4180.parse_stream(file_stream)
-
-      frequency_map =
-        Enum.reduce(csv_stream, %{}, fn row, acc ->
-
-          if length(row) > max(neighbourhood_index, property_type_index) do
-            neighbourhood = Enum.at(row, neighbourhood_index, "")
-            property_type = Enum.at(row, property_type_index, "")
-
-            key = {neighbourhood, property_type}
-            Map.update(acc, key, 1, &(&1 + 1))
-          else
-
-            acc
-          end
-        end)
-
-      result_list =
-        Enum.map(frequency_map, fn {{neighbourhood, type}, count} ->
-          %{colonia: neighbourhood, tipo: type, total: count}
-        end)
-
-      result_list
-
+      filename
+      |> File.stream!([:read, :utf8], :line)
+      |> NimbleCSV.RFC4180.parse_stream()
+      |> Enum.map(&process_row_for_offer(&1, min_row_length, neighbourhood_index, accommodates_index, price_index))
+      |> Enum.filter(&match?({:ok, _}, &1))
+      |> Enum.map(&elem(&1, 1))
+      |> Enum.group_by(&elem(&1, 0))
+      |> Enum.map(&calculate_offer_aggregates/1)
     rescue
       e in File.Error -> {:error, "File error: #{inspect(e)}"}
       e -> {:error, "An unexpected error occurred: #{inspect(e)}"}
     end
+  end
+
+  defp process_row_for_offer(row, min_len, n_idx, acc_idx, p_idx) do
+    if length(row) >= min_len do
+      neighbourhood = Enum.at(row, n_idx)
+      acc_str = Enum.at(row, acc_idx)
+      price_str = Enum.at(row, p_idx)
+
+      with {:ok, accommodates} <- parse_accommodates(acc_str),
+           {:ok, price} <- parse_price(price_str) do
+
+        price_per_person = if accommodates > 0, do: price / accommodates, else: nil
+
+        {:ok, {neighbourhood, accommodates, price_per_person}}
+      else
+        _error -> :error
+      end
+    else
+      :error
+    end
+  end
+
+  defp parse_accommodates(acc_str) when is_binary(acc_str) do
+    case Integer.parse(acc_str) do
+      {int_val, ""} -> {:ok, int_val}
+      _ -> :error
+    end
+  end
+  defp parse_accommodates(_), do: :error
+
+  defp parse_price(price_str) when is_binary(price_str) do
+    cleaned_str = String.replace(price_str, ["$", ","], "")
+    parse_result = Float.parse(cleaned_str)
+
+    case parse_result do
+      {float_val, ""} -> {:ok, float_val}
+      _ -> :error
+    end
+  end
+  defp parse_price(_), do: :error
+
+  defp calculate_offer_aggregates({neighbourhood, properties_data}) do
+
+    total_capacity = Enum.sum(for {_, acc, _} <- properties_data, do: acc)
+    valid_ppps = for {_, _, ppp} <- properties_data, not is_nil(ppp), do: ppp
+
+    average_ppp =
+      if Enum.empty?(valid_ppps) do
+        0.0
+      else
+        avg = Enum.sum(valid_ppps) / Enum.count(valid_ppps)
+        Float.round(avg, 2)
+      end
+
+    %{
+      neighbourhood: neighbourhood,
+      hosting_capacity: total_capacity,
+      average_price_per_person: average_ppp
+    }
   end
 end
